669bd1bf5b1c0377ddfa92d560b09be4fedc7159
thangbk2209 <thangbk2209@gmail.com>
2018-02-06 08:51:38 +0700

    3 metric with time steps

--- models/3Metric_mem.py
+++ models/3Metric_mem.py
@@ -43,9 +43,7 @@ for sliding in sliding_widow:
 		datai=[]
 		for j in range(sliding):
 			datai.append(CPU_nomal[i+j])
-		for j in range(sliding):
 			datai.append(RAM_nomal[i+j])
-		for j in range(sliding):
 			datai.append(disk_space_normal[i+j])
 		data.append(datai)
 	data = np.array(data)
@@ -62,8 +60,8 @@ for sliding in sliding_widow:
 
 	# reshape input to be [samples, time steps, features]
 
-	trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
-	testX = np.reshape(testX, (testX.shape[0], testX.shape[1], 1))
+	trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1]/3, 3))
+	testX = np.reshape(testX, (testX.shape[0], testX.shape[1]/3, 3))
 	print 'trainX reshape'
 	print trainX
 	for batch_size in batch_size_array: 
@@ -71,7 +69,7 @@ for sliding in sliding_widow:
 		print "batch_size= ", batch_size
 		# model 1 layer 4 neural
 		model = Sequential()
-		model.add(LSTM(512,return_sequences=True, activation = 'relu',input_shape=(3*sliding,1)))
+		model.add(LSTM(512,return_sequences=True, activation = 'relu',input_shape=(sliding,3)))
 		model.add(LSTM(4, activation = 'relu'))
 		model.add(Dense(1, activation = 'relu'))
 
@@ -91,7 +89,7 @@ for sliding in sliding_widow:
 		plt.xlabel('epoch')
 		plt.legend(['train', 'test'], loc='upper left')
 		# plt.show()
-		plt.savefig('lastResults/3Metric/mem/history_sliding=%s_batchsize=%s_optimize=sgd.png'%(sliding,batch_size))
+		plt.savefig('lastResults/3Metric/mem_timesteps/history_sliding=%s_batchsize=%s_optimize=sgd.png'%(sliding,batch_size))
 		testPredict = model.predict(testX)
 
 		print len(testPredict), len(testY)
@@ -106,9 +104,9 @@ for sliding in sliding_widow:
 		print('Test Score: %.6f MAE' % (testScoreMAE))
 		
 		testDf = pd.DataFrame(np.array(testPredictInverse))
-		testDf.to_csv('lastResults/3Metric/mem/testPredictInverse_sliding=%s_batchsize=%s_optimize=sgd.csv'%(sliding,batch_size), index=False, header=None)
+		testDf.to_csv('lastResults/3Metric/mem_timesteps/testPredictInverse_sliding=%s_batchsize=%s_optimize=sgd.csv'%(sliding,batch_size), index=False, header=None)
 		errorScore=[]
 		errorScore.append(testScoreRMSE)
 		errorScore.append(testScoreMAE)
 		errorDf = pd.DataFrame(np.array(errorScore))
-		errorDf.to_csv('lastResults/3Metric/mem/error_sliding=%s_batchsize=%s_optimize=sgd.csv'%(sliding,batch_size), index=False, header=None)
+		errorDf.to_csv('lastResults/3Metric/mem_timesteps/error_sliding=%s_batchsize=%s_optimize=sgd.csv'%(sliding,batch_size), index=False, header=None)
